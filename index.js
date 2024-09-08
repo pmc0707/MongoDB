167039cc68dca48633dca8ffd506c01bd01b9c2d
pmc0707 <pranshugoodboy@gmail.com>
2024-09-08 14:53:00 +0530

    connection of server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/" ,(req,res) => {
+    res.send("root working")
+})
 app.listen(8080, ()=> {
     console.log("server is created")
 })
