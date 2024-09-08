8eecde91ab8267d6c9dbd197d249868700d11285
pmc0707 <pranshugoodboy@gmail.com>
2024-09-08 15:15:27 +0530

    provide data type to chat

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+main().then(() => {
+    console.log("connection succesful")
+})
+.catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
 
 app.get("/" ,(req,res) => {
     res.send("root working")
---
+++ models/chat.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const chatScehema = new mongoose.Schema({
+    from :{
+        type: String,
+        require: true
+    },
+    to :{
+        type :String,
+        require :true
+    },
+    msg :{
+        type:String,
+        maxLength : 50,
+        require :true
+    },
+    created_at :{
+        type :Date,
+        require :true
+    }
+})
